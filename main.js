137b09073083f6a1d6913f0840b61e2923dcd932
NEBULA1978 <rpascualey@gmail.com>
2023-10-26 01:00:29 +0200

    Separo archivos html con javascript me falta separar  Card Banner 1 y footer

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
+// Función para cargar y agregar el contenido HTML desde un archivo a un elemento del DOM
+function loadHTMLContent(file, elementId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById(elementId).innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+// Cargar el encabezado, la barra de navegación y el pie de página
+loadHTMLContent('header.html', 'header-container');
+loadHTMLContent('nav.html', 'nav-container');
+loadHTMLContent('footer.html', 'footer-container');
+// Cargar el contenido de news-cards.html
+loadHTMLContent('news-cards.html', 'news-cards-container');
